79856f966ff4ab3a35c0c7554483cb03b4f85da8
Szynkie <saicxs123@gmail.com>
2024-11-07 20:19:18 +0100

    changing text to PL

--- Program.cs
+++ Program.cs
@@ -1,13 +1,5 @@
-using ChristmasLottery.Database;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace ChristmasLottery
 {
@@ -15,7 +7,8 @@ namespace ChristmasLottery
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var app = CreateHostBuilder(args).Build();
+            app.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
